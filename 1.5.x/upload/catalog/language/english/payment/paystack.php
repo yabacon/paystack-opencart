3631e4adda942de1745e16dafafe26727617a547
ibrahimlawal <ibrahim@lawal.me>
2016-06-13 13:35:19 +0100

    Short name for Gateway

--- 1.5.x/upload/catalog/language/english/payment/paystack.php
+++ 1.5.x/upload/catalog/language/english/payment/paystack.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title'] = 'Credit or Debit Card (Processed securely by Paystack)';
+$_['text_title'] = 'Paystack';
 $_['text_testmode'] = 'Warning: The payment gateway is in \'Test Mode\'. Only a  test card can be used.';
